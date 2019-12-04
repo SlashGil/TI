a3ec1e3cfd3324131cc6fb72d787888316480a33
Salvador Gil <slash_gil@outlook.com>
2019-12-03 22:10:24 -0600

    Version 0.3

---
+++ app/src/main/java/com/chava/ti/Activities/versus.java
@@ -0,0 +1,16 @@
+package com.chava.ti.Activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.chava.ti.R;
+
+public class versus extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_versus);
+    }
+}
---
+++ app/src/main/java/com/chava/ti/interfaces/IComunicateFragments.java
@@ -0,0 +1,11 @@
+package com.chava.ti.interfaces;
+
+public interface IComunicateFragments {
+
+    public void launchFaq();
+    public void launchVersus();
+    public void launchClaustro();
+    public void launchGroups();
+    public void launchFacebook();
+    public void launchInsta();
+}
